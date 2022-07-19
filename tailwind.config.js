bdc31057c385e9992dc24cd231160e7bb7037ee1
Kyle Wigley <mail@kylewigs.com>
2022-07-19 09:28:58 -0400

    package update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const plugin = require("tailwindcss/plugin");
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.html", "./app/**/*.{js,jsx,ts,tsx,md,mdx}"],
   darkMode: "media",
@@ -19,54 +21,6 @@ module.exports = {
           },
         },
       },
-      // // This mess until dark mode is added to @tailwindcss/typography
-      // typography: ({ theme }) => ({
-      //   DEFAULT: {
-      //     css: {
-      //       "pre code": {
-      //         paddingRight: "24px",
-      //       },
-      //
-      //       "--tw-prose-body": theme("colors.gray[500]"),
-      //       "--tw-prose-invert-body": theme("colors.gray[400]"),
-      //       "--tw-prose-links": theme("colors.blue[600]"),
-      //       "--tw-prose-invert-links": theme("colors.blue[300]"),
-      //       "--tw-prose-invert-pre-bg": "#080c13",
-      //     },
-      //   },
-      //   green: {
-      //     css: {
-      //       "--tw-prose-body": theme("colors.green[700]"),
-      //       "--tw-prose-invert-body": theme("colors.green[200]"),
-      //       "--tw-prose-links": theme("colors.green[700]"),
-      //       "--tw-prose-invert-links": theme("colors.green[200]"),
-      //     },
-      //   },
-      //   blue: {
-      //     css: {
-      //       "--tw-prose-body": theme("colors.blue[700]"),
-      //       "--tw-prose-invert-body": theme("colors.blue[200]"),
-      //       "--tw-prose-links": theme("colors.blue[700]"),
-      //       "--tw-prose-invert-links": theme("colors.blue[200]"),
-      //     },
-      //   },
-      //   yellow: {
-      //     css: {
-      //       "--tw-prose-body": theme("colors.yellow[700]"),
-      //       "--tw-prose-invert-body": theme("colors.yellow[200]"),
-      //       "--tw-prose-links": theme("colors.yellow[700]"),
-      //       "--tw-prose-invert-links": theme("colors.yellow[200]"),
-      //     },
-      //   },
-      //   red: {
-      //     css: {
-      //       "--tw-prose-body": theme("colors.red[700]"),
-      //       "--tw-prose-invert-body": theme("colors.red[200]"),
-      //       "--tw-prose-links": theme("colors.red[700]"),
-      //       "--tw-prose-invert-links": theme("colors.red[200]"),
-      //     },
-      //   },
-      // }),
     },
   },
   plugins: [
